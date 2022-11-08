528319f5ad5d055c5fb88ca6316accddc7f2d6e0
Migram05 <migram05@ucm.es>
2022-11-08 14:18:44 +0100

    Sistema de luz y sombra
    Primera versión del sistema de iluminacion

--- src/scenes/mainLevel.js
+++ src/scenes/mainLevel.js
@@ -17,10 +17,12 @@ export default class mainLevel extends Phaser.Scene {
 	}
 	preload(){
         this.load.image('player', 'assets/Hero/player.png');
+        this.load.image('cone', 'assets/Hero/cone.png');
         this.load.image('floor', 'assets/floor.png');
     }
     create(){
         var ground = this.add.image(310,200,'floor');
+        
         // graphic object used to draw walls
         this.wallGraphics = this.add.graphics();
         this.wallGraphics.lineStyle(1, 0x00ff00);
@@ -31,13 +33,14 @@ export default class mainLevel extends Phaser.Scene {
         // array with all polygons in game
         this.polygons = [];
  
-        // add random boxes
-        //for(let i = 0; i < gameOptions.boxes; i ++){
-        //    this.addRandomBox();
-        //}
+        this.enemies = [];
         let player = new Player(this, 300, 150);
         this.enemy = new Enemy(this, 400, 100, player);
-
+        this.enemy2 = new Enemy(this, 200, 100, player);
+        this.enemies.push(this.enemy);
+        this.enemies.push(this.enemy2);
+        
+        this.cone = this.add.image(1200, 1200, 'cone');
         player.body.onCollide = true; 
         var escena = this;
         this.physics.add.collider(player, this.enemy, onCollision);
@@ -51,33 +54,27 @@ export default class mainLevel extends Phaser.Scene {
         var width = 50;
         var height = 50;
         //this.wallGraphics.strokeRect(startX, startY, width, height);
-        this.polygons[0] = ([[this.enemy.x, this.enemy.y], [this.enemy.x + 40, this.enemy.y], [this.enemy.x + 40, this.enemy.y + 40], [this.enemy.x, this.enemy.y + 40]]);
+        for(let i=0; i< this.enemies.length; i++){
+            this.polygons[i] = ([[this.enemies[i].x, this.enemies[i].y], [this.enemies[i].x + 40, this.enemies[i].y], [this.enemies[i].x + 40, this.enemies[i].y + 40], [this.enemies[i].x, this.enemies[i].y + 40]]);
+        }
+        
 
-        this.polygons[1]=([[startX, startY], [startX + width, startY], [startX + width, startY + height], [startX, startY + height]]);
+        this.polygons.push([[startX, startY], [startX + width, startY], [startX + width, startY + height], [startX, startY + height]]);
         // walls around game perimeter
-        //this.polygons[2]=([[player.x -100, player.y -100], [player.x +100, player.y -100], [player.x +100, player.y +100], [player.x -100, player.y +100]]);
 		this.polygons.push([[-1, -1], [this.sys.game.canvas.width + 1, -1], [this.sys.game.canvas.width + 1, this.sys.game.canvas.height+1], [-1, this.sys.game.canvas.height + 1]]);
-       
-        //this.renderLight(player);
-    }
-    
-    updateEnemy(enemy){
-        this.polygons[0] = ([[enemy.x-10, enemy.y-10], [enemy.x + 10, enemy.y-10], [enemy.x + 10, enemy.y + 10], [enemy.x-10, enemy.y + 10]]);
     }
 	updatePlayer(player){
-        let ang1 = (this.enemy.rotation* (180/Math.PI));
-        let ang2 = (player.rotation * (180/Math.PI));
-        var calc = Math.abs(ang1-ang2);
-        if((calc >=160 && calc <=180) || (calc<=200 && calc >=180)) this.enemy.detente();
-        else this.enemy.continua();
-
-		var startX = 200;
-        var startY = 200;
-        var width = 50;
-        var height = 50;
-        this.polygons[1] = ([[startX, startY], [startX + width, startY], [startX + width, startY + height], [startX, startY + height]]);
-		//this.polygons[2]=([[player.x -100, player.y -200],[player.x , player.y -220], [player.x +100, player.y -200],[player.x +120, player.y -150], [player.x +20, player.y +20], [player.x -20, player.y +20],[player.x -120, player.y -150]]);
-		//this.polygons[2] =([[player.x -200, player.y -200],[player.x +200 , player.y -200], [player.x +10, player.y +10], [player.x -10, player.y +10]]);
+        this.cone.x = player.x;
+        this.cone.y = player.y;
+        this.cone.rotation = player.rotation;
+        for(let i=0; i< this.enemies.length; i++){
+            let ang1 = (this.enemies[i].rotation* (180/Math.PI));
+            let ang2 = (player.rotation * (180/Math.PI));
+            var calc = Math.abs(ang1-ang2);
+            if((calc >=160 && calc <=180) || (calc<=200 && calc >=180)) this.enemies[i].detente();
+            else this.enemies[i].continua();
+            this.polygons[i] = ([[this.enemies[i].x-10, this.enemies[i].y-10], [this.enemies[i].x + 10, this.enemies[i].y-10], [this.enemies[i].x + 10, this.enemies[i].y + 10], [this.enemies[i].x-10, this.enemies[i].y + 10]]);
+        }
 	}
     // method to render the light
     renderLight(player){
