a300f064c665ab2fad32f51fdbf13d23e007f58f
Nahia Iglesias <nahigles@ucm.es>
2022-11-25 18:06:28 +0100

    Clase power up creada

---
+++ src/gameObjects/powerUp.js
@@ -0,0 +1,25 @@
+export default class PowerUp extends Phaser.GameObjects.Sprite{
+
+    constructor(scene, x, y, type){
+
+        super(scene, x, y, "powerUp");
+
+		this.scene.add.existing(this);
+		this.setOrigin(1,1);
+		this.setScale(2,2);
+
+        this.tipo = type;
+    }
+
+    create ()
+    {
+        
+
+    }
+
+    preUpdate(t, dt){
+		//super.preUpdate(t, dt); // Muy importante llamar al preUpdate del padre (Sprite) para que se ejecute la animación
+		
+
+	}
+}
