69b779948b7a0cab9c06b69f848b267185d85118
Paula López <paulal18@ucm.es>
2022-12-15 23:05:13 +0100

    autores página

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function aparecerTexto()
     var controlsChildren = controls.children;
     //Texto
     var historiaT = document.getElementById("Historia");
+    //Autores
+    var autores = document.getElementById("autoresText");
     //Activamos
     if(historiaT.style.visibility !== "visible")
     {
@@ -20,16 +22,58 @@ function aparecerTexto()
                 e.style.visibility = "hidden";
             }
         }
+        if(autores.style.visibility !== "hidden")
+        {
+            autores.style.visibility = "hidden";
+        }
     }
     //Desactivamos
     else  historiaT.style.visibility = "hidden";
     
 };
+//Activamos y desactivamos el texto de autores
+function aparecerAutores()
+{
+    //Imágenes de los controles
+    var controls = document.getElementById("controlesImgs");
+    var controlsChildren = controls.children;
+    //Texto
+    var historiaT = document.getElementById("Historia");
+    //Autores
+    var autores = document.getElementById("autoresText");
+    //Activamos
+    if(autores.style.visibility !== "visible")
+    {
+        //Activamos texto
+        autores.style.visibility = "visible";
+        //Desactivamos controles e historia por si acaso 
+        if(controls.style.visibility !== "hidden")
+        {
+            controls.style.visibility = "hidden";
+            for(let e of controlsChildren) {
+                
+                e.style.visibility = "hidden";
+            }
+        }
+        if(historiaT.style.visibility !== "hidden")
+        {
+            historiaT.style.visibility = "hidden";
+        }
+    }
+    //Desactivamos
+    else  autores.style.visibility = "hidden";
+    console.log(autores.style.visibility);
+    
+};
 
 function aparecerControles()
 {
-    var controls = document.getElementById("controlesImgs");
+    //Historia
     var historiaT = document.getElementById("Historia");
+    //Autores
+    var autores = document.getElementById("autoresText");
+    //Controles
+    var controls = document.getElementById("controlesImgs");
     var controlsChildren = controls.children;
     //Activamos
     if(controls.style.visibility !== "visible")
@@ -39,6 +83,11 @@ function aparecerControles()
         if(historiaT.style.visibility !== "hidden")
         {
             historiaT.style.visibility = "hidden";
+        }
+        //Si los autores son visibles, los quitamos
+        if(autores.style.visibility !== "hidden")
+        {
+            autores.style.visibility = "hidden";
         }
         //Mostramos las imágenes
         for(let e of controlsChildren) {
